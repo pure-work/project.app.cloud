0cd1e777412450aedfb688767b4084d43faf63f9
ThreeMountain <593583264@qq.com>
2018-11-26 23:51:48 +0800

    author cwf

--- account-service/src/main/java/com/app/cloud/web/HelloWorldController.java
+++ account-service/src/main/java/com/app/cloud/web/HelloWorldController.java
@@ -1,23 +1,20 @@
 package com.app.cloud.web;
 
-import com.app.cloud.web.hystrix.AccountClient;
+import com.app.cloud.web.feign.Account;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
 
 @RestController
 public class HelloWorldController {
 
     @Autowired
-    private AccountClient accountClient;
+    private Account account;
 
     @RequestMapping(value = "/demoAdd")
     public String add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b) {
-        return "accountClient result is "+accountClient.add(a, b)
+        return "accountClient result is "+account.add(a, b)
                 +",consumerClient random is "+Math.random();
     }
 }
--- account-service/src/main/java/com/app/cloud/web/feign/AccountFault.java
+++ account-service/src/main/java/com/app/cloud/web/feign/AccountFault.java
@@ -1,13 +1,12 @@
-package com.app.cloud.web.hystrix;
+package com.app.cloud.web.feign;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//@Component("accountClientFallback")
 @Component
-public class AccountClientFallback implements AccountClient{
+public class AccountFault implements Account{
 
-    //@Override
+    @Override
     public String add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b) {
         return "add method call failed";
     }
