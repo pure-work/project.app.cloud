a02f641c4e808f10759d370432a67c45deeb2d41
ThreeMountain <593583264@qq.com>
2018-11-26 23:51:23 +0800

    author cwf

--- account-service/src/main/java/com/app/cloud/web/feign/Account.java
+++ account-service/src/main/java/com/app/cloud/web/feign/Account.java
@@ -1,12 +1,11 @@
-package com.app.cloud.web.hystrix;
+package com.app.cloud.web.feign;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "accountClient", fallback = AccountClientFallback.class)
-public interface AccountClient {
+@FeignClient(name = "account", fallback = AccountFault.class)
+public interface Account {
 
     @RequestMapping(value = "/add")
     String add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
